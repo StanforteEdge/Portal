8c3d187f695d1f9dc643ae71c441a4743aa6ca5f
EDGD Media <77864273+edgdmedia@users.noreply.github.com>
2026-02-19 00:18:18 +0100

    Delete plugin directory

--- plugin/Core/User/Routes/Admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-// DEPRECATED: Admin user routes have moved to Modules/admin/Routes/users.php
-// This file is intentionally disabled to avoid duplicate route registration.
-// If included by mistake, return immediately.
-return;
-
-// (Intentionally left disabled)
